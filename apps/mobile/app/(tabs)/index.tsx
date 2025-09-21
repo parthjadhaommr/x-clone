e5351942bd124602e7f50ae21386fbed7f69f82b
parthjadhaommr <parthjadhao.mmr@gmail.com>
2025-09-21 14:23:16 +0530

    feat : homescreen fetch post and like functionality implemented

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import SignOutButton from '@/components/SignOutButton';
 import { useUserSync } from "@/hooks/useUserSync";
 import { Ionicons } from '@expo/vector-icons';
 import PostComposer from '@/components/PostComposer';
+import PostList from '@/components/PostList';
 
 
 const HomeScreen = () => {
@@ -23,6 +24,7 @@ const HomeScreen = () => {
                 contentContainerStyle={{ paddingBottom: 80 }}
             >
                 <PostComposer></PostComposer>
+                <PostList></PostList>
             </ScrollView>
         </SafeAreaView>
     )
