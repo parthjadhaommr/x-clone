f37ce37f51ab26fc49f5715b162c55b635b7e2e5
parthjadhaommr <parthjadhao.mmr@gmail.com>
2025-09-21 09:52:20 +0530

    feat : implemented user db and clerk sync

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
-import {Text } from 'react-native'
+import { Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
 import SignOutButton from '@/components/SignOutButton';
+import { useUserSync } from "@/hooks/useUserSync";
+
 
 const HomeScreen = () => {
+    useUserSync()
+
     return (
         <SafeAreaView>
             <Text>HomeScreen</Text>
